b6d6eeb631e621b8f5f9c5e80b81d461f4e4a6dc
WHQ <2283149823@qq.com>
2019-05-07 18:10:24 +0800

    Update TaskMode.vue

--- front-end/src/components/TaskMode.vue
+++ front-end/src/components/TaskMode.vue
@@ -41,7 +41,7 @@
           <br>
           <h1 v-show="selected" style="margin-top: 10px;">任务时间</h1>
           <div style="margin-top: 10px; margin-bottom: 10px; margin-left: 20px;">
-            <h2>至{{currentDeadline}}</h2>
+            <h2>{{currentStarttime}}至{{currentDeadline}}</h2>
           </div>
           <h1 v-show="selected">任务详情</h1>
           <div style="margin-top: 20px">
@@ -113,6 +113,7 @@ export default {
       currentCondition: false,
       currentStatus: -2,
       currentDeadline: null,
+      currentStarttime:null,
       taskData: [],
       // taskRequestUrl: "http://localhost:8080/task/getTask",
       // taskStartUrl: "http://localhost:8080/task/startTask",
@@ -298,6 +299,7 @@ export default {
       // this.currentTotalPomo = parseInt(msg.expectedTomato);
       this.currentTaskDetail = msg.description;
       this.currentDeadline = msg.finishtime;
+      this.currentStarttime=msg.starttime;
       this.currentStatus = msg.status;
       this.currentCondition = false;
       // if (
